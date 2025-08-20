ad38b729681c55b22f4958b45709d4a5eff5d16e
everyfun89 <mamiemoema@gmail.com>
2025-08-20 15:45:04 +0200

    Update products.js

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,18 +1,6 @@
-import dbConnect from "@/lib/mongodb";
-import Product from "@/models/Product";
-
-export default async function handler(req, res) {
-  await dbConnect();
-
-  if (req.method === "GET") {
-    const products = await Product.find({});
-    return res.status(200).json(products);
-  }
-
-  if (req.method === "POST") {
-    const product = await Product.create(req.body);
-    return res.status(201).json(product);
-  }
-
-  res.status(405).end(); // Method not allowed
+export default function handler(req, res) {
+  res.status(200).json([
+    { name: "Demo Product 1", price: 10 },
+    { name: "Demo Product 2", price: 20 }
+  ]);
 }
