b45c24fe38e184e490ee08667a4952774bc3f57d
everyfun89 <mamiemoema@gmail.com>
2025-08-20 15:08:46 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welkom bij Creative Tools Shop 🛠️</h1>
+      <p>Dit is je nieuwe webshop met Next.js & MongoDB.</p>
+    </div>
+  );
+}
