a4e9e1152d90f0b4ab487db07596b1d9f67b8e9f
everyfun89 <mamiemoema@gmail.com>
2025-08-21 12:01:25 +0200

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,2 +1,12 @@
-// models/Product.js
-export default {}; // dummy export
+import mongoose from 'mongoose';
+
+const ProductSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: String,
+  price: { type: Number, required: true },
+  imageUrl: String,
+  category: { type: String, enum: ['Crafts', 'Drawing'], required: true },
+  stock: { type: Number, default: 0 },
+}, { timestamps: true });
+
+export default mongoose.models.Product || mongoose.model('Product', ProductSchema);
