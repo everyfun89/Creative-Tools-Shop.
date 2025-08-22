94ccefe56ba970467e42d298993707c4c3bed115
everyfun89 <mamiemoema@gmail.com>
2025-08-22 14:54:39 +0200

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,3 +1,19 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "Auth is disabled for demo" });
-}
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import EmailProvider from "next-auth/providers/email";
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+});
