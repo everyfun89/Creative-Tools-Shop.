8cd0181cd94e55b50e1cb40bdff46094de7ee5a9
everyfun89 <mamiemoema@gmail.com>
2025-08-23 16:08:00 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,2 +1,11 @@
 // models/User.js
-export default {}; // dummy export
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, default: "" },
+  email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.models.User || mongoose.model("User", UserSchema);
