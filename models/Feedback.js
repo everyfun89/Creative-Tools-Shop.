ea9a9ef93113a86a60995b1f49505673af64fd52
everyfun89 <mamiemoema@gmail.com>
2025-08-24 12:54:04 +0200

    Update Feedback.js

--- models/Feedback.js
+++ models/Feedback.js
@@ -1,7 +1,10 @@
+// models/Feedback.js
+import mongoose from "mongoose";
+
 const FeedbackSchema = new mongoose.Schema({
   name: { type: String, default: "Anonymous" },
-  email: { type: String, default: "" },
-  rating: { type: Number, min: 1, max: 5, required: true },
-  message: { type: String, default: "" },
+  message: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
 });
+
+export default mongoose.models.Feedback || mongoose.model("Feedback", FeedbackSchema);
