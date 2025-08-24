3de8e591b80e136751889f878df334b44b02c9c2
everyfun89 <mamiemoema@gmail.com>
2025-08-24 12:47:03 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,15 @@ module.exports = {
       },
       boxShadow: {
         soft: "0 4px 10px rgba(0,0,0,0.05)"
+      },
+      keyframes: {
+        "slide-up": {
+          "0%": { transform: "translateY(100%)", opacity: 0 },
+          "100%": { transform: "translateY(0)", opacity: 1 }
+        }
+      },
+      animation: {
+        "slide-up": "slide-up 0.5s ease-out"
       }
     }
   },
