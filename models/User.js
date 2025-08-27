ce9b481f273b0a1a7ad72643e6e1a18136be3f9f
everyfun89 <mamiemoema@gmail.com>
2025-08-27 10:56:01 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,13 +1,10 @@
-// models/User.js
 import mongoose from "mongoose";
 
-// Schema definitie
 const UserSchema = new mongoose.Schema({
   name: { type: String, default: "" },
   email: { type: String, required: true, unique: true, lowercase: true, trim: true },
   password: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
 });
 
-// Check of model al bestaat (Next.js hot reload probleem vermijden)
 export default mongoose.models.User || mongoose.model("User", UserSchema);
